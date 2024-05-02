cd804b7c01cf364f186fdfce59bb382b4135982d
m-mueller678 <marcus.mueller.678@gmail.com>
2024-05-02 17:12:32 +0200

    always print size

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,7 @@ fn main() {
     let rounded_size = pointer_count * size_of::<*const u8>();
     assert!(pointer_count > 0);
     if x.print_header {
-        if x.print_size {
-            print!("size,");
-        }
-        println!("time,latency",);
+        println!("size,time,latency",);
     }
     if x.exit == Some(0) {
         return;
@@ -36,11 +33,8 @@ fn main() {
                 if now.duration_since(start).as_millis() >= (x.time * (print_count + 1)) as u128 {
                     let time_per_pointer = duration.as_nanos() as f64 / iterations as f64;
                     print_count += 1;
-                    if x.print_size {
-                        print!("{rounded_size},");
-                    }
                     println!(
-                        "{},{time_per_pointer}",
+                        "{rounded_size},{},{time_per_pointer}",
                         now.duration_since(start).as_secs_f64()
                     );
                     if Some(print_count) == x.exit {
@@ -67,9 +61,6 @@ struct Cli {
     /// Exit after printing N measurements
     #[arg(short, long)]
     exit: Option<u64>,
-    /// Print size in addition to recorded timings.
-    #[arg(long)]
-    print_size: bool,
     /// Print header
     #[arg(long)]
     print_header: bool,
