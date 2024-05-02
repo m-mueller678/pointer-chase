f9364e99a0fb780e70fdafdd4d0153bd9247ddda
m-mueller678 <marcus.mueller.678@gmail.com>
2024-05-02 15:40:40 +0200

    add options to print header and size

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
 use crate::pointer_cycle::PointerCycle;
 use clap::Parser;
 use minstant::Instant;
+use std::mem::size_of;
 
 mod pointer_cycle;
 
 fn main() {
     let x = Cli::parse();
-    let pointer_count = x.size / 8;
+    let pointer_count = x.size / size_of::<*const u8>();
+    let rounded_size = pointer_count * size_of::<*const u8>();
     assert!(pointer_count > 0);
+    if x.print_header {
+        if x.print_size {
+            print!("size,");
+        }
+        println!("time,latency",);
+    }
+    if x.exit == Some(0) {
+        return;
+    }
+
     let cycle = PointerCycle::build(pointer_count);
     let start = Instant::now();
     let mut ref_time = start;
@@ -21,11 +33,14 @@ fn main() {
         if duration.as_millis() >= x.time as u128 {
             let time_per_pointer = duration.as_nanos() / (iterations * pointer_count) as u128;
             print_count += 1;
+            if x.print_size {
+                print!("{rounded_size},");
+            }
             println!(
                 "{},{time_per_pointer}",
                 now.duration_since(start).as_secs_f64()
             );
-            if x.exit != 0 && print_count >= x.exit {
+            if Some(print_count) == x.exit {
                 break;
             } else {
                 ref_time = now;
@@ -44,7 +59,13 @@ struct Cli {
     /// Duration in milliseconds
     #[arg(short, long, default_value = "1000")]
     time: u64,
-    /// Exit after printing N measurements, 0 to run indefinitely
-    #[arg(short, long, default_value = "0")]
-    exit: usize,
+    /// Exit after printing N measurements
+    #[arg(short, long)]
+    exit: Option<usize>,
+    /// Print size in addition to recorded timings.
+    #[arg(long)]
+    print_size: bool,
+    /// Print header
+    #[arg(long)]
+    print_header: bool,
 }
